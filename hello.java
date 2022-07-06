e516da30b199ac8c110af6a1391c86e861a644d5
Galib <nafizmdgalib35@gmail.com>
2022-07-06 09:36:25 +0600

    Added a new method called display

--- hello.java
+++ hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args){
         System.out.printIn("Vs Code is Awesome...");
+
+        greeting("Nafiz MD Galib")
+    }
+
+    public static void display(String name){
+        System.out.printIn("Welcome" + name);
     }
 }
