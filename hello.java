1ed1e65062efd3401a4160ab5d58248d493537d0
Galib <nafizmdgalib35@gmail.com>
2022-07-07 08:36:43 +0600

    all method removed and add message method

--- hello.java
+++ hello.java
@@ -1,15 +1,9 @@
 public class Hello {
     public static void main(String[] args){
         System.out.printIn("Vs Code is Awesome...");
-
-        display()
+    }
+    public static void message(){
+        System.out.printIn("Hurray!!! Now we are able to contribute on open source...");
     }
 
-    public static void display(){
-        System.out.printIn("Hello Hello");
-    }
-
-    public static int sqr(int x) {
-       return x*x;
-    }
 }
